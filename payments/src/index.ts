f2c1ec56b2a4faed726caa084956519531547790
haridalavai <dalaavai.hari@gmail.com>
2021-10-17 17:30:13 +0530

    change deploy

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,8 @@ import { app } from "./app";
 import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 const start = async () => {
+  console.log("pay change");
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
